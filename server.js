1f7abdcf7f4b0d9e281032e5194eb01ca260bd65
AhmedHamza667 <hamzaa667788@gmail.com>
2023-07-11 15:53:27 -0400

    done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const books = require('./books');
+//const books = require('./books');
 require('dotenv').config();
 const { query } = require('./database');
 
@@ -20,6 +20,31 @@ app.get("/", (req, res) => {
   });
 
 
+app.get("/books", async (req, res) => {
+  try {
+    const allBooks = await query("SELECT * FROM books");
+    res.status(200).json(allBooks.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+
+  try {
+    const book = await query("SELECT * FROM books WHERE id = $1", [bookId]);
+
+    if (book.rows.length > 0) {
+      res.status(200).json(book.rows[0]);
+    } else {
+      res.status(404).send({ message: "Book not found" });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 app.post("/books", async (req, res) => {
   const { id, title, author, publishdate } = req.body;
 
@@ -36,6 +61,46 @@ app.post("/books", async (req, res) => {
   }
 });
 
+
+app.patch("/books/:id", async (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+
+  const { id, title, author, publishdate } = req.body;
+
+  try {
+    const updatedBook = await query(
+      "UPDATE books SET id = $1, title = $2, author = $3, publishdate = $4 WHERE id = $5 RETURNING *",
+      [bookId, title, author, publishdate, bookId]
+    );
+
+    if (updatedBook.rows.length > 0) {
+      res.status(200).json(updatedBook.rows[0]);
+    } else {
+      res.status(404).send({ message: "Book not found" });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+
+app.delete("/books/:id", async (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+
+  try {
+    const deleteOp = await query("DELETE FROM books WHERE id = $1", [bookId])
+
+    if (deleteOp.rowCount > 0) {
+      res.status(200).send({ message: "Book deleted" });
+    } else {
+      res.status(404).send({ message: "Book not found" });
+    }
+  } catch (err) {
+      console.error(err);
+    }
+  });
+
+  
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
